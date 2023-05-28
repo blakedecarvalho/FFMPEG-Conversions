e0247f07f4b57c4f65c7b62fa25ef57af3f44b8c
Blake DeCarvalho <malbolge@tuta.io>
2023-05-28 09:43:42 -0400

    Adding CPU color
    Added color to CPU batch file for quick glances

--- LOCALUPGRADER-CPU.bat
+++ LOCALUPGRADER-CPU.bat
@@ -10,6 +10,7 @@ for /r "%cd%" %%a in (*.mp4 *.avi *.mkv *.flv) do (
         cls
     ) || (
         if not exist "!upscaled!" (
+		color 17
             echo Currently working on:
             echo "!file!"
             ffmpeg -hwaccel cuda -i "!file!" -vf scale=3840x2160:flags=bicubic  -rc constqp -qmin 17 -qmax 51 -qp 24 -rc-lookahead 4 -bf 0 -keyint_min 1 -refs 7 -qdiff 20 -qcomp 0.9 -me_method umh -c:v libx264 -async 1 -threads 4 -c:a copy "!upscaled!" 
@@ -18,11 +19,13 @@ for /r "%cd%" %%a in (*.mp4 *.avi *.mkv *.flv) do (
 		cls
             for %%F in ("!upscaled!") do (
                 if %%~zF EQU 0 (
+			  color 47
                     echo new file is 0KB. Skipping deletion of old.
 			  del /q "!upscaled!"
 			  timeout /t 5
 			  cls
                 ) else (
+			  color 27
                     echo New file looks good.  Deleting original
                     del /q "!file!"
                     timeout /t 5
@@ -36,5 +39,6 @@ for /r "%cd%" %%a in (*.mp4 *.avi *.mkv *.flv) do (
         )
     )
 )
+color E0
 echo All done. Press any key to exit.
 pause >nul
