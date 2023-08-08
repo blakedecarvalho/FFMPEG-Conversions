f3b44d9336f1cbdebdae03d71704632fc6f0285a
Blake DeCarvalho <malbolge@tuta.io>
2023-08-08 15:21:55 -0400

    Split into CPU vs GPU

--- LOCALUPGRADER-GPU.bat
+++ LOCALUPGRADER-GPU.bat
@@ -1,6 +1,20 @@
 @ECHO OFF
 setlocal enabledelayedexpansion
 set "root=%cd%"
+set total=0
+set count=1
+rem Building Total count
+for /r "%cd%" %%a in (*.mp4 *.avi *.mkv *.flv) do (
+    set "file=%%~dpnxa"
+    set "upscaled=%%~dpa%%~na_upscaled%%~xa"
+    echo "!file!" | findstr /i /c:"_upscaled" >nul || (
+        if not exist "!upscaled!" (
+            set /a total+=1
+        )
+    )
+)
+
+rem Doing main task
 for /r "%cd%" %%a in (*.mp4 *.avi *.mkv *.flv) do (
     set "file=%%~dpnxa"
     set "upscaled=%%~dpa%%~na_upscaled%%~xa"
@@ -10,28 +24,29 @@ for /r "%cd%" %%a in (*.mp4 *.avi *.mkv *.flv) do (
         cls
     ) || (
         if not exist "!upscaled!" (
-		color 17
-            echo Currently working on:
+            color 17
+            echo Currently working on: !count!/!total!
             echo "!file!"
             ffmpeg -hwaccel cuda -i "!file!" -vf scale=3840x2160:flags=bicubic -rc constqp -qmin 17 -qmax 51 -qp 24 -preset p7 -tune hq -rc-lookahead 4 -profile:v high -bf 0 -keyint_min 1 -refs 7 -qdiff 20 -qcomp 0.9 -me_method umh -c:v h264_nvenc -async 1 -threads 4 -c:a copy "!upscaled!" -loglevel quiet
             echo Checking MP4 file size..
-			timeout /t 5
-			cls
+            timeout /t 5
+            cls
             for %%F in ("!upscaled!") do (
                 if %%~zF EQU 0 (
-			  color 47
+                    color 47
                     echo new file is 0KB. Skipping deletion of old.
-			  del /q "!upscaled!"
-			  timeout /t 5
-			  cls
-                ) else (
-			  color 27
-                    echo New file looks good.  Deleting original
-			  del /q "!file!"
+                    del /q "!upscaled!"
                     timeout /t 5
-			  cls
+                    cls
+                ) else (
+                    color 27
+                    echo New file looks good.  Deleting original
+                    del /q "!file!"
+                    timeout /t 5
+                    cls
                 )
-			)
+            )
+            set /a count+=1
         ) else (
             echo Skipped already upscaled file:
             echo "!file!"
@@ -39,6 +54,7 @@ for /r "%cd%" %%a in (*.mp4 *.avi *.mkv *.flv) do (
         )
     )
 )
+
 color E0
 echo All done. Press any key to exit.
 pause >nul
